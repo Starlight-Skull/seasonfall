e3ad91ad1e530980af8b378a32ca361579b0c855
sander.vanhoutte <sander.van.houtte@hotmail.be>
2021-11-30 17:12:38 +0100

    ev listeners for debug

--- src/script.js
+++ src/script.js
@@ -1,5 +1,19 @@
 ;(function () {
     'use strict';
 
+    window.addEventListener('load', function () {
 
+    });
+
+    document.addEventListener('keydown', ev => {
+        if (ev.key === '`') {
+            document.getElementById('debug').hidden = false;
+        }
+    });
+
+    document.addEventListener('keyup', ev => {
+        if (ev.key === '`') {
+            document.getElementById('debug').hidden = true;
+        }
+    });
 })();
