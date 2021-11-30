3eddda0b804345345dc1c5ea43e01297c1af98eb
sander.vanhoutte <sander.van.houtte@hotmail.be>
2021-11-30 21:03:34 +0100

    api call interval

--- src/script.js
+++ src/script.js
@@ -1,19 +1,37 @@
 ;(function () {
     'use strict';
 
+    const apiKey = '4042c10a2f6340a71458988530a305fd';
+    const units = 'metric';
+    const location = 'Ghent,BE';
+
+    function getWeather(location) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${location}&units=${units}`)
+            .then(res => {
+                return res.json();
+            }).then(json => {
+            console.log(json);
+        });
+    }
+
     window.addEventListener('load', function () {
+        const debug = document.getElementById('debug');
+        const screen = document.getElementById('screen');
 
-    });
+        document.addEventListener('keypress', ev => {
+            if (ev.key === '`') {
+                debug.hidden = !debug.hidden;
+            }
+        });
 
-    document.addEventListener('keydown', ev => {
-        if (ev.key === '`') {
-            document.getElementById('debug').hidden = false;
-        }
-    });
+        document.getElementById('refreshButton').addEventListener('click', () => {
+            getWeather(location);
+        });
 
-    document.addEventListener('keyup', ev => {
-        if (ev.key === '`') {
-            document.getElementById('debug').hidden = true;
-        }
+        getWeather(location);
+        setInterval(() => {
+            getWeather(location);
+            // todo add UI icon
+        }, 600000);
     });
 })();
