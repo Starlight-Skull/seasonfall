35eab3ed948a9dd8facb02e6acf94c23fa921f09
Starlight-Skull <74898654+Starlight-Skull@users.noreply.github.com>
2021-12-08 00:45:29 +0100

    split classes, class extensions, missing animation

--- src/app/classes.js
+++ src/app/classes.js
@@ -1,22 +1,23 @@
 export class entity {
-    constructor(hasCollision, cooldown, speed, maxHP, maxMP, maxAir, x, y, sprite, width, height, sX, sY, sWidth, sHeight, frames, currentFrame) {
+    constructor(hasCollision, cooldown, speed, maxHP, maxMP, maxAir, x, y, width, height) {
         this.cooldown = cooldown || -1;
+        let sprite = new Image();
+        sprite.src = './img/missing.png';
+        this.missing = new spriteSet(sprite, 0, 0, 36, 36, 1, 'missing');
         this.frame = {
             x: x || 0,
             y: y || 0,
             width: width || 150,
             height: height || 250,
-            sprite: sprite,
-            sX: sX,
-            sY: sY,
-            sWidth: sWidth,
-            sHeight: sHeight,
-            frames: frames,
-            currentFrame: currentFrame,
-            mirrored: false
+            currentFrame: 1,
+            mirrored: false,
         };
+        this.animation = this.missing;
+        this.move = this.missing;
+        this.attack = this.missing;
+        this.idle = this.missing;
         this.stats = {
-            hp: maxHP|| 100,
+            hp: maxHP || 100,
             maxHP: maxHP || 100,
             mp: maxMP || 0,
             maxMP: maxMP || 0,
@@ -24,7 +25,7 @@ export class entity {
         }
         this.speed = speed || 10;
         this.air = 0;
-        this.maxAir = maxAir || 25;
+        this.maxAir = maxAir || 15;
         this.controls = {
             up: false,
             down: false,
@@ -42,6 +43,19 @@ export class entity {
     }
 }
 
+export class hero extends entity {
+    constructor(x, y) {
+        super(true, -1, 10, 100, 25, 15, x, y, 110, 155);
+
+        let sprite = new Image();
+        sprite.src = './img/kain_animations.png';
+
+        this.idle = new spriteSet(sprite, 36, 0, 36, 36, 1, 'idle');
+        this.move = new spriteSet(sprite, 36, 0, 36, 36, 13, 'move');
+        this.attack = new spriteSet(sprite, 36, 36, 36, 36, 3, 'attack');
+    }
+}
+
 export class tile {
     constructor(hasCollision, color, x, y, width, height) {
         this.frame = {
@@ -51,6 +65,18 @@ export class tile {
             height: height || 150
         }
         this.color = color || 'black';
-        this.hasCollision = hasCollision;
+        this.hasCollision = hasCollision; // 2 = only top collision
+    }
+}
+
+export class spriteSet {
+    constructor(sprite, x, y, width, height, frames, name) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+        this.frames = frames;
+        this.width = width;
+        this.height = height;
+        this.name = name;
     }
 }
--- src/app/globals.js
+++ src/app/globals.js
@@ -1,21 +1,23 @@
-import {entity, tile} from "./classes.js";
-import {playerSprite} from "./sprites.js";
+import {entity, hero, tile} from "./classes.js";
 
-export const player = new entity(true, -1, 10, 100, 25, 15, 0, 0, playerSprite, 110, 155, 36, 0, 36, 36, 12, 0);
+export let debug = {
+    showBoxes: false,
+};
 
-export const npcList = [
-    new entity(true, 20, 6, 25, 15, 150, 0, 100, '', 50, 50),
-    new entity(true, 20, 2, 50, 0, 5, 0, 100, '', 150, 200)
+export const player = new hero(0, 0);
+
+export const entityList = [
+    new entity(true, 20, 6, 25, 15, 150, 0, 100, 50, 50),
+    new entity(true, 20, 2, 50, 0, 5, 0, 100, 150, 200)
 ];
 
-export const border = [
+export const tileList = [
+    // border
     new tile(true, 'brown', -40, 0, 50, screen.height),
     new tile(true, 'brown', screen.width - 10, 0, 50, screen.height),
     new tile(true, 'green', 0, -40, screen.width, 50),
-    new tile(true, 'cyan', 0, screen.height - 10, screen.width, 50)
-]
-
-export const tileList = [
+    new tile(true, 'cyan', 0, screen.height - 10, screen.width, 50),
+    // tiles
     new tile(true, 'black', 500, 0, 300, 100),
     new tile(true, 'black', 1050, 500, 450, 150),
     new tile(false, 'yellow', 700, 550, 350, 100),
