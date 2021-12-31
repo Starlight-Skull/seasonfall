d96375ca5ffda86852d0356e6b167d256d9d7753
Starlight-Skull <74898654+Starlight-Skull@users.noreply.github.com>
2021-12-31 10:47:42 +0100

    enemies can die now

--- src/app/movement.js
+++ src/app/movement.js
@@ -14,167 +14,183 @@ export function entityMovement(entity) {
     if (entity.frame.y < 0) {
         entity.frame.y = 0;
     }
-    if (entity !== player) {
-        if (entity.cooldown > 0) {
-            entity.cooldown--;
-        } else if (entity.cooldown !== -1) {
-            if (Math.abs(entity.frame.x - player.frame.x) < 500) {
-                if (player.frame.y - entity.frame.y > 0 && entity.air !== entity.maxAir) {
-                    entity.controls.jump = true;
-                    entity.controls.down = false;
-                } else if (entity.frame.y - player.frame.y > 0) {
-                    entity.controls.jump = false;
-                    entity.controls.down = true;
-                } else {
-                    entity.controls.jump = false;
-                    entity.controls.down = false;
+    if (entity.animation === entity.death && entity.stats.hp <= 0) {
+        if (entity.frame.currentFrame < entity.animation.frames - 1) {
+            entity.frame.currentFrame += entity.animation.speed;
+        }
+        entity.controls.up = false;
+        entity.controls.jump = false;
+        entity.controls.down = false;
+        entity.controls.left = false;
+        entity.controls.right = false;
+        for (let i = 0; i < tileList.length; i++) {
+            collision(entity, tileList[i]);
+        }
+        if (!entity.collision.down && entity.hasCollision) {
+            entity.frame.y -= 9.81 * 2;
+        }
+    } else {
+        if (entity !== player) {
+            if (entity.cooldown > 0) {
+                entity.cooldown--;
+            } else if (entity.cooldown !== -1) {
+                if (Math.abs(entity.frame.x - player.frame.x) < 500) {
+                    if (player.frame.y - entity.frame.y > 0 && entity.air !== entity.maxAir) {
+                        entity.controls.jump = true;
+                        entity.controls.down = false;
+                    } else if (entity.frame.y - player.frame.y > 0) {
+                        entity.controls.jump = false;
+                        entity.controls.down = true;
+                    } else {
+                        entity.controls.jump = false;
+                        entity.controls.down = false;
+                    }
+                    if (entity.frame.x - (player.frame.x + player.frame.width) > 0 && (entity.frame.x - (player.frame.x + player.frame.width) < 500)) {
+                        entity.controls.left = true;
+                        entity.controls.right = false;
+                    } else if (player.frame.x - (entity.frame.x + entity.frame.width) > 0 && (player.frame.x - (entity.frame.x + entity.frame.width) < 500)) {
+                        entity.controls.left = false;
+                        entity.controls.right = true;
+                    } else {
+                        entity.controls.left = false;
+                        entity.controls.right = false;
+                    }
+                    entity.cooldown = 20;
                 }
-                if (entity.frame.x - (player.frame.x + player.frame.width) > 0 && (entity.frame.x - (player.frame.x + player.frame.width) < 500)) {
-                    entity.controls.left = true;
-                    entity.controls.right = false;
-                } else if (player.frame.x - (entity.frame.x + entity.frame.width) > 0 && (player.frame.x - (entity.frame.x + entity.frame.width) < 500)) {
-                    entity.controls.left = false;
-                    entity.controls.right = true;
-                } else {
-                    entity.controls.left = false;
-                    entity.controls.right = false;
-                }
-                entity.cooldown = 20;
             }
         }
-    }
-    entity.collision.up = false;
-    entity.collision.down = false;
-    entity.collision.left = false;
-    entity.collision.right = false;
-    for (let i = 0; i < tileList.length; i++) {
-        collision(entity, tileList[i]);
-    }
-    for (let i = 0; i < tileEntityList.length; i++) {
-        collision(entity, tileEntityList[i]);
-    }
-
-    if (entity.air > 0 && entity.air <= entity.maxAir && !entity.controls.jump) {
-        if (entity.animation !== entity.fall) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.fall;
+        entity.collision.up = false;
+        entity.collision.down = false;
+        entity.collision.left = false;
+        entity.collision.right = false;
+        for (let i = 0; i < tileList.length; i++) {
+            collision(entity, tileList[i]);
         }
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        } else {
-            entity.frame.currentFrame = 0;
+        for (let i = 0; i < tileEntityList.length; i++) {
+            collision(entity, tileEntityList[i]);
         }
-    }
-    if (entity.collision.down && entity.air !== 0) {
-        if (!entity.controls.left && !entity.controls.right) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.idle;
+        if (entity.collision.down && entity.air !== 0) {
+            if (!entity.controls.left && !entity.controls.right) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.idle;
+            }
+            if (!entity.controls.jump) {
+                entity.air = 0;
+            }
         }
-        if (!entity.controls.jump) {
+        if (entity.controls.right && !entity.collision.right && !(entity.controls.up && (entity.air === 0 || entity.air === entity.maxAir))) {
+            entity.frame.x += entity.stats.speed;
+            if (entity.animation !== entity.move) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.move;
+            }
+            entity.frame.mirrored = false;
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            } else {
+                entity.frame.currentFrame = 0;
+            }
+        }
+        if (entity.controls.left && !entity.collision.left && !(entity.controls.up && (entity.air === 0 || entity.air === entity.maxAir))) {
+            entity.frame.x -= entity.stats.speed;
+            if (entity.animation !== entity.move) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.move;
+            }
+            entity.frame.mirrored = true;
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            } else {
+                entity.frame.currentFrame = 0;
+            }
+        }
+        if (((entity.controls.left && entity.controls.right) || (!entity.controls.left && !entity.controls.right)) && !entity.controls.up && entity.animation !== entity.fall && !entity.controls.jump) {
+            if (entity.animation !== entity.idle) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.idle;
+            }
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            } else {
+                entity.frame.currentFrame = 0;
+            }
+        }
+        if (entity.controls.jump && (entity.air < entity.maxAir) && !entity.collision.up && entity.hasCollision) {
+            entity.frame.y += 20;
+            entity.air++;
+            if (entity.animation !== entity.jump) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.jump;
+            }
+            entity.frame.currentFrame = entity.air / entity.maxAir * entity.animation.frames - 1;
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            }
+        } else if (((entity.air > entity.maxAir) || (!entity.controls.jump && entity.air > (entity.maxAir / 2)) || !entity.collision.down) && entity.hasCollision) {
+            entity.frame.y -= 9.81 * 2;
+            entity.air = entity.maxAir;
+            if (entity.animation !== entity.fall) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.fall;
+            }
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            } else {
+                entity.frame.currentFrame = 0;
+            }
+        }
+        if (!entity.hasCollision) {
             entity.air = 0;
+            if (entity.controls.jump) {
+                entity.frame.y += entity.stats.speed;
+            }
+            if (entity.controls.down) {
+                entity.frame.y -= entity.stats.speed;
+            }
         }
-    }
-
-    if (entity.controls.right && !entity.collision.right && !(entity.controls.up && (entity.air === 0 || entity.air === entity.maxAir))) {
-        entity.frame.x += entity.stats.speed;
-        if (entity.animation !== entity.move) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.move;
-        }
-        entity.frame.mirrored = false;
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        } else {
-            entity.frame.currentFrame = 0;
-        }
-    }
-    if (entity.controls.left && !entity.collision.left && !(entity.controls.up && (entity.air === 0 || entity.air === entity.maxAir))) {
-        entity.frame.x -= entity.stats.speed;
-        if (entity.animation !== entity.move) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.move;
-        }
-        entity.frame.mirrored = true;
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        } else {
-            entity.frame.currentFrame = 0;
-        }
-    }
-    if (((entity.controls.left && entity.controls.right) || (!entity.controls.left && !entity.controls.right)) && !entity.controls.up && entity.animation !== entity.fall && !entity.controls.jump) {
-        if (entity.animation !== entity.idle) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.idle;
-        }
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        } else {
-            entity.frame.currentFrame = 0;
-        }
-    }
-    if (entity.controls.jump && (entity.air < entity.maxAir) && (!entity.collision.up) && entity.hasCollision) {
-        entity.frame.y += (entity.stats.speed * 2);
-        entity.air++;
-        if (entity.animation !== entity.jump) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.jump;
-        }
-        entity.frame.currentFrame = entity.air / entity.maxAir * entity.animation.frames - 1;
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        }
-    } else if (((entity.air > entity.maxAir) || (!entity.controls.jump && entity.air > 0) || !entity.collision.down) && entity.hasCollision) {
-        entity.frame.y -= 9.81 * 2;
-        entity.air = entity.maxAir;
-    }
-    if (!entity.hasCollision) {
-        entity.air = 0;
-        if (entity.controls.jump) {
-            entity.frame.y += entity.stats.speed;
-        }
-        if (entity.controls.down) {
-            entity.frame.y -= entity.stats.speed;
-        }
-    }
-    entity.collision.up = false;
-    entity.collision.down = false;
-    entity.collision.left = false;
-    entity.collision.right = false;
-    if (entity.controls.up === true) {
-        if (entity.animation !== entity.attack) {
-            entity.frame.currentFrame = 0;
-            entity.animation = entity.attack;
-        }
-        if (entity.frame.currentFrame < entity.animation.frames - 1) {
-            entity.frame.currentFrame += entity.animation.speed;
-        } else {
-            for (let i = 0; i < entityList.length; i++) {
-                let ent;
-                if (entity.constructor.name === 'Hero') {
-                    ent = collision(entity, entityList[i], true);
-                } else {
-                    ent = collision(entity, player, true);
-                }
-                if (ent && entity.frame.mirrored ? entity.collision.left : entity.collision.right) {
-                    // console.log(ent)
-                    if (ent.stats.hp > 0) {
-                        ent.stats.hp -= entity.stats.damage;
-                    } else if (ent.stats.hp <= 0) {
-                        entityList.splice(entityList.indexOf(ent), 1);
-                        entity.stats.xp += ent.stats.xp;
+        entity.collision.up = false;
+        entity.collision.down = false;
+        entity.collision.left = false;
+        entity.collision.right = false;
+        if (entity.controls.up === true) {
+            if (entity.animation !== entity.attack) {
+                entity.frame.currentFrame = 0;
+                entity.animation = entity.attack;
+            }
+            if (entity.frame.currentFrame < entity.animation.frames - 1) {
+                entity.frame.currentFrame += entity.animation.speed;
+            } else {
+                for (let i = 0; i < entityList.length; i++) {
+                    let ent;
+                    if (entity.constructor.name === 'Hero') {
+                        ent = collision(entity, entityList[i], true);
+                    } else {
+                        ent = collision(entity, player, true);
+                    }
+                    if (ent && (entity.frame.mirrored ? entity.collision.left : entity.collision.right)) {
+                        console.log(ent.stats.hp)
+                        if (ent.stats.hp > 0) {
+                            ent.stats.hp -= entity.stats.damage;
+                        }
+                        if (ent.stats.hp <= 0 && ent.animation !== ent.death) {
+                            // entityList.splice(entityList.indexOf(ent), 1);
+                            ent.animation = ent.death;
+                            ent.frame.currentFrame = 0;
+                            entity.stats.xp += ent.stats.xp;
+                        }
                     }
                 }
-            }
-            for (let i = 0; i < tileEntityList.length; i++) {
-                let til;
-                if (entity.constructor.name === 'Hero') {
-                    til = collision(entity, tileEntityList[i], true);
+                for (let i = 0; i < tileEntityList.length; i++) {
+                    let til;
+                    if (entity.constructor.name === 'Hero') {
+                        til = collision(entity, tileEntityList[i], true);
+                    }
+                    if (til) {
+                        til.activate();
+                    }
                 }
-                if (til) {
-                    til.activate();
-                }
+                entity.controls.up = 2;
             }
-            entity.controls.up = 2;
         }
     }
 }
