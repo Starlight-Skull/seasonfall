d096276ef040424feef65ab7a53c29572ab4fba4
StarlightSkull <sander.van.houtte@hotmail.be>
2022-01-02 20:53:43 +0100

    added request handlers

--- api/app.js
+++ api/app.js
@@ -1,31 +1,43 @@
+// express
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
+// bodyparser & cors
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const cors = require("cors");
 app.use(cors());
 
-app.get('/hello', function(req, res){
-    res.send('Hello World!')
+// requests
+const stats = require('./requests/requests_stats');
+const {Request_stats} = require("./requests/requests_stats");
+
+app.get('/stats/', (req, res) => {
+    stats.getAllStats()
+        .then((records) => {
+            res.setHeader("Content-Type", "application/json");
+            res.send(records);
+        }).catch((error) => {
+        res.send({error: error, status: 500});
+    });
 });
 
-app.get('', function (req, res) {
-
+app.post('/stats/', (req, res) => {
+    const body = req.body;
+    if (!body.hasOwnProperty("user")) {
+        return res.send({error: "invalid input", status: 422});
+    }
+    stats.postStats(
+        new Request_stats(body.user, body.timeTaken, body.kills, body.attacks, body.attacksHit, body.damageTaken, body.damageDealt)
+    ).then(() => {
+        res.send({
+            body: body,
+            message: 'post successful',
+            status: 201
+        });
+    });
 });
 
-app.post('', function (req, res) {
-
+app.listen(port, () => {
 });
-
-app.put('', function (req, res) {
-
-});
-
-app.delete('', function (req, res) {
-
-});
-
-app.listen(port, function () {});
