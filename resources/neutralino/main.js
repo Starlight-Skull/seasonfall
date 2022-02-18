654c8d39225467160a9de67431fc370d90895d1e
sander.vanhoutte <sander.van.houtte@hotmail.be>
2022-02-19 00:28:38 +0100

    reconfigured many neutralino settings
    Took 1 hour 41 minutes

--- resources/neutralino/main.js
+++ resources/neutralino/main.js
@@ -1,23 +1,10 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
-
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
-
 function setTray() {
     if(NL_MODE !== "window") {
         console.log("INFO: Tray menu is only available in the window mode.");
         return;
     }
     let tray = {
-        icon: "/resources/img/favicon.ico",
+        icon: "/resources/icons/icon_16x.png",
         menuItems: [
             {id: "VERSION", text: "Get version"},
             {id: "SEP", text: "-"},
@@ -39,15 +26,9 @@ function onTrayMenuItemClicked(event) {
     }
 }
 
-function onWindowClose() {
-    Neutralino.app.exit();
-}
-
 Neutralino.init();
-
 Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", onWindowClose);
 
-if(NL_OS !== "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
+if(NL_OS !== "Darwin") {
     setTray();
 }
