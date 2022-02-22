528a49cd711d92d50d755993cafbdba4523ce2eb
StarlightSkull <74898654+starlight-skull@users.noreply.github.com>
2022-02-22 14:47:36 +0100

    cleaned up menu functions
    Took 2 minutes

--- resources/app/data.js
+++ resources/app/data.js
@@ -9,41 +9,37 @@ window.addEventListener('load', function () {
     fromStorage('debug');
     // event listeners
     window.addEventListener('mousedown', () => {
-        if (debugMenu.style.visibility === 'hidden' && !world.paused) {
+        if (!world.paused) {
             player.controls.attack = (player.controls.attack === 2) ? 2 : true;
         }
     });
     window.addEventListener('mouseup', () => {
-        if (debugMenu.style.visibility === 'hidden' && !world.paused) {
+        if (!world.paused) {
             player.controls.attack = false;
         }
     });
     window.addEventListener('keydown', ev => {
-        if (debugMenu.style.visibility === 'hidden' && !world.paused) {
+        if (!world.paused) {
             keyLogger(ev);
         }
     });
     window.addEventListener('keyup', ev => {
         switch (ev.key) {
             case '`':
-                if (debugMenu.style.visibility === 'hidden') {
-                    openDebug();
-                } else {
-                    debugMenu.style.visibility = 'hidden';
-                }
+                openDebugMenu();
                 break;
             case 'Escape':
-                escapeMenu();
+                openPauseMenu();
                 break;
         }
-        if (debugMenu.style.visibility === 'hidden' && !world.paused) {
+        if (!world.paused) {
             keyLogger(ev);
         }
     });
 
-    document.getElementById('continueButton').addEventListener('click', escapeMenu);
+    document.getElementById('continueButton').addEventListener('click', openPauseMenu);
 
-    function escapeMenu() {
+    function openPauseMenu() {
         if (pauseMenu.style.visibility === 'hidden') {
             world.paused = true;
             pauseMenu.style.visibility = 'visible';
@@ -107,71 +103,72 @@ window.addEventListener('load', function () {
             });
     }
 
-    document.getElementById('update').addEventListener('click', () => {
-        // general
-        debug.userId = document.getElementById('userId').value;
-        debug.username = document.getElementById('username').value;
-        debug.showBoxes = document.getElementById('showBoxes').checked;
-        debug.showLiveDebug = document.getElementById('showLiveDebug').checked;
-        debug.showPlayerStats = document.getElementById('showPlayerStats').checked;
-        // weather
-        weather.main = document.getElementById('main').value;
-        weather.temp = parseFloat(document.getElementById('temp').value);
-        weather.windSpeed = parseFloat(document.getElementById('windSpeed').value);
-        weather.windDeg = document.getElementById('windDeg').value;
-        weather.clouds = parseFloat(document.getElementById('clouds').value);
-        weather.rain = parseFloat(document.getElementById('rain').value);
-        weather.snow = parseFloat(document.getElementById('snow').value);
-        weather.time = parseInt(document.getElementById('time').value);
-        weather.sunrise = parseInt(document.getElementById('sunrise').value);
-        weather.sunset = parseInt(document.getElementById('sunset').value);
-        // player
-        player.stats.hp = parseFloat(document.getElementById('hp').value);
-        player.stats.maxHP = parseFloat(document.getElementById('maxHp').value);
-        player.stats.mp = parseFloat(document.getElementById('mp').value);
-        player.stats.maxMP = parseFloat(document.getElementById('maxMp').value);
-        player.stats.xp = parseFloat(document.getElementById('xp').value);
-        player.stats.damage = parseFloat(document.getElementById('damage').value);
-        player.stats.speed = parseFloat(document.getElementById('speed').value);
-        player.maxAir = parseFloat(document.getElementById('maxAir').value);
-        player.frame.x = parseFloat(document.getElementById('x').value);
-        player.frame.y = parseFloat(document.getElementById('y').value);
-        player.hasCollision = document.getElementById('hasCollision').checked;
-        debugMenu.style.visibility = 'hidden';
-    });
-
-    function openDebug() {
-        debugMenu.style.visibility = 'visible';
-        document.getElementById('update').disabled = false;
-        // general
-        document.getElementById('userId').value = debug.userId;
-        document.getElementById('username').value = debug.username;
-        document.getElementById('showBoxes').checked = debug.showBoxes;
-        document.getElementById('showLiveDebug').checked = debug.showLiveDebug;
-        document.getElementById('showPlayerStats').checked = debug.showPlayerStats;
-        // weather
-        document.getElementById('main').value = weather.main;
-        document.getElementById('temp').value = weather.temp;
-        document.getElementById('windSpeed').value = weather.windSpeed;
-        document.getElementById('windDeg').value = weather.windDeg;
-        document.getElementById('clouds').value = weather.clouds;
-        document.getElementById('rain').value = weather.rain;
-        document.getElementById('snow').value = weather.snow;
-        document.getElementById('time').value = weather.time;
-        document.getElementById('sunrise').value = weather.sunrise;
-        document.getElementById('sunset').value = weather.sunset;
-        // player
-        document.getElementById('hp').value = player.stats.hp;
-        document.getElementById('maxHp').value = player.stats.maxHP;
-        document.getElementById('mp').value = player.stats.mp;
-        document.getElementById('maxMp').value = player.stats.maxMP;
-        document.getElementById('xp').value = player.stats.xp;
-        document.getElementById('damage').value = player.stats.damage;
-        document.getElementById('speed').value = player.stats.speed;
-        document.getElementById('maxAir').value = player.maxAir;
-        document.getElementById('x').value = player.frame.x;
-        document.getElementById('y').value = player.frame.y;
-        document.getElementById('hasCollision').checked = player.hasCollision;
+    function openDebugMenu() {
+        if (debugMenu.style.visibility === 'hidden') {
+            world.paused = true;
+            debugMenu.style.visibility = 'visible';
+            // general
+            document.getElementById('userId').value = debug.userId;
+            document.getElementById('username').value = debug.username;
+            document.getElementById('showBoxes').checked = debug.showBoxes;
+            document.getElementById('showLiveDebug').checked = debug.showLiveDebug;
+            document.getElementById('showPlayerStats').checked = debug.showPlayerStats;
+            // weather
+            document.getElementById('main').value = weather.main;
+            document.getElementById('temp').value = weather.temp;
+            document.getElementById('windSpeed').value = weather.windSpeed;
+            document.getElementById('windDeg').value = weather.windDeg;
+            document.getElementById('clouds').value = weather.clouds;
+            document.getElementById('rain').value = weather.rain;
+            document.getElementById('snow').value = weather.snow;
+            document.getElementById('time').value = weather.time;
+            document.getElementById('sunrise').value = weather.sunrise;
+            document.getElementById('sunset').value = weather.sunset;
+            // player
+            document.getElementById('hp').value = player.stats.hp;
+            document.getElementById('maxHp').value = player.stats.maxHP;
+            document.getElementById('mp').value = player.stats.mp;
+            document.getElementById('maxMp').value = player.stats.maxMP;
+            document.getElementById('xp').value = player.stats.xp;
+            document.getElementById('damage').value = player.stats.damage;
+            document.getElementById('speed').value = player.stats.speed;
+            document.getElementById('maxAir').value = player.maxAir;
+            document.getElementById('x').value = player.frame.x;
+            document.getElementById('y').value = player.frame.y;
+            document.getElementById('hasCollision').checked = player.hasCollision;
+        } else {
+            // general
+            debug.userId = document.getElementById('userId').value;
+            debug.username = document.getElementById('username').value;
+            debug.showBoxes = document.getElementById('showBoxes').checked;
+            debug.showLiveDebug = document.getElementById('showLiveDebug').checked;
+            debug.showPlayerStats = document.getElementById('showPlayerStats').checked;
+            // weather
+            weather.main = document.getElementById('main').value;
+            weather.temp = parseFloat(document.getElementById('temp').value);
+            weather.windSpeed = parseFloat(document.getElementById('windSpeed').value);
+            weather.windDeg = document.getElementById('windDeg').value;
+            weather.clouds = parseFloat(document.getElementById('clouds').value);
+            weather.rain = parseFloat(document.getElementById('rain').value);
+            weather.snow = parseFloat(document.getElementById('snow').value);
+            weather.time = parseInt(document.getElementById('time').value);
+            weather.sunrise = parseInt(document.getElementById('sunrise').value);
+            weather.sunset = parseInt(document.getElementById('sunset').value);
+            // player
+            player.stats.hp = parseFloat(document.getElementById('hp').value);
+            player.stats.maxHP = parseFloat(document.getElementById('maxHp').value);
+            player.stats.mp = parseFloat(document.getElementById('mp').value);
+            player.stats.maxMP = parseFloat(document.getElementById('maxMp').value);
+            player.stats.xp = parseFloat(document.getElementById('xp').value);
+            player.stats.damage = parseFloat(document.getElementById('damage').value);
+            player.stats.speed = parseFloat(document.getElementById('speed').value);
+            player.maxAir = parseFloat(document.getElementById('maxAir').value);
+            player.frame.x = parseFloat(document.getElementById('x').value);
+            player.frame.y = parseFloat(document.getElementById('y').value);
+            player.hasCollision = document.getElementById('hasCollision').checked;
+            world.paused = false;
+            debugMenu.style.visibility = 'hidden';
+        }
     }
 
     function callAPI() {
