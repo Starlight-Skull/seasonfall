9f21c1c4c04545d56d4f056c5a7e55c8c5b9b610
StarlightSkull <74898654+starlight-skull@users.noreply.github.com>
2022-02-26 14:16:07 +0100

    added JSdocs and shortened some functions
    Took 59 minutes

--- resources/app/init.js
+++ resources/app/init.js
@@ -1,53 +1,74 @@
 Neutralino.init();
-if (NL_OS !== "Darwin") setTray();
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
+setupTray();
 
+/**
+ * Toggles the fullscreen when F11 is pressed.
+ */
 window.addEventListener('keydown', event => {
-    if (event.key === 'F11') {
-        Neutralino.window.isFullScreen().then(fullscreen => {
-            if (fullscreen) Neutralino.window.exitFullScreen().then(Neutralino.window.unmaximize().then(Neutralino.window.maximize()));
-            else Neutralino.window.setFullScreen();
-        });
-    }
+    if (event.key === 'F11') Neutralino.window.isFullScreen().then(fullscreen => {
+        if (fullscreen) {
+            Neutralino.window.exitFullScreen()
+                .then(Neutralino.window.unmaximize()
+                    .then(Neutralino.window.maximize()));
+        }
+        else Neutralino.window.setFullScreen();
+    });
 });
 
-function setTray() {
-    if (NL_MODE !== "window") {
-        console.log("INFO: Tray menu is only available in the window mode.");
-        return;
+/**
+ * Sets up the tray menu if the app is in window mode and not on macOS.
+ */
+function setupTray() {
+    if (NL_OS !== "Darwin" && NL_MODE === "window") {
+        Neutralino.os.setTray({
+            icon: "/resources/icons/icon_16x.png",
+            menuItems: [
+                {id: "GIT", text: "Open on GitHub"},
+                {id: "SEP-VER", text: "-"},
+                {id: "VER-APP", text: `Seasonfall: v${NL_APPVERSION}`},
+                {id: "VER-SERVER", text: `Neutralino server: v${NL_VERSION}`},
+                {id: "VER-CLIENT", text: `Neutralino client: v${NL_CVERSION}`},
+                {id: "SEP", text: "-"},
+                {id: "QUIT", text: "Quit"}
+            ]
+        });
+        Neutralino.events.on("trayMenuItemClicked", event => {
+            switch (event.detail.id) {
+                case "GIT":
+                    Neutralino.os.open('https://github.com/Starlight-Skull/Seasonfall');
+                    break;
+                case "QUIT":
+                    Neutralino.app.exit();
+                    break;
+            }
+        });
     }
-    let tray = {
-        icon: "/resources/icons/icon_16x.png",
-        menuItems: [
-            {id: "GIT", text: "Open on GitHub"},
-            {id: "SEP-VER", text: "-"},
-            {id: "VER-APP", text: `Seasonfall: v${NL_APPVERSION}`},
-            {id: "VER-SERVER", text: `Neutralino server: v${NL_VERSION}`},
-            {id: "VER-CLIENT", text: `Neutralino client: v${NL_CVERSION}`},
-            {id: "SEP", text: "-"},
-            {id: "QUIT", text: "Quit"}
-        ]
-    };
-    Neutralino.os.setTray(tray);
 }
 
-function onTrayMenuItemClicked(event) {
-    switch (event.detail.id) {
-        case "GIT":
-            Neutralino.os.open('https://github.com/Starlight-Skull/Seasonfall');
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
-    }
+/**
+ * Exits the app.
+ * @returns {Promise<unknown>}
+ */
+export async function quit() {
+    await Neutralino.app.exit();
 }
 
+/**
+ * Returns data for the given key from Neutralino storage.
+ * @param key - The name of requested key.
+ * @returns {Promise<unknown>} - A promise with the requested data.
+ */
 export async function fromStorage(key) {
-    return await Neutralino.storage.getData(key).then(data => {
-        return JSON.parse(data);
-    });
+    return await Neutralino.storage.getData(key).then(JSON.parse);
 }
 
+/**
+ * Saves a given key-value pair to Neutralino storage.
+ * This data is persisted in the .storage directory.
+ * @param key - The name of the key to store the value as.
+ * @param value - The value to store. If this is null or undefined, the record will be erased.
+ * @returns {Promise<unknown>} - Returns a promise if the data has been stored.
+ */
 export async function toStorage(key, value) {
     return await Neutralino.storage.setData(key, JSON.stringify(value));
 }
