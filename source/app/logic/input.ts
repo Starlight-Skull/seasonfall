4571bb14c0c304500aa6fca2fe51f3351e653f21
Starlight-Skull <74898654+Starlight-Skull@users.noreply.github.com>
2025-08-13 19:56:29 +0200

    split up collision

--- source/app/logic/input.ts
+++ source/app/logic/input.ts
@@ -1,5 +1,33 @@
-import { world, player } from '../globals'
+import { world, player, settings } from '../globals'
 import { render } from '../rendering/common'
+
+/**
+ * Receives the code of a mouse or key event and acts accordingly.
+ * @param key - Can be 'KeyboardEvent.code' or 'Mouse + MouseEvent.button'.
+ * @param down - Boolean of whether the event is up or down.
+ */
+export function handleMouseKeyEvent(key: string, down: boolean): void {
+  switch (key) {
+    case settings.keybindings.attack:
+      if (!world.paused) player.movement.attack = down
+      break
+    case settings.keybindings.down:
+      if (!world.paused) player.movement.down = down
+      break
+    case settings.keybindings.left:
+      if (!world.paused) player.movement.left = down
+      break
+    case settings.keybindings.right:
+      if (!world.paused) player.movement.right = down
+      break
+    case settings.keybindings.jump:
+      if (!world.paused) player.movement.jump = down
+      break
+    case settings.keybindings.use:
+      if (!world.paused) player.movement.use = down
+      break
+  }
+}
 
 // onmousedown = (e: MouseEvent) => {
 //   if (!world.paused) {
