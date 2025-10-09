421c148ed7c6af6905642e9e4829c38de7a44177
Starlight-Skull <74898654+Starlight-Skull@users.noreply.github.com>
2025-10-10 00:52:17 +0200

    remove mana healing

--- source/app/classes/Entity/Skeleton.ts
+++ source/app/classes/Entity/Skeleton.ts
@@ -4,7 +4,7 @@ import SpriteSet from '../SpiteSet'
 export default class Skeleton extends Entity {
   constructor (x: number, y: number) {
     const skeleton = 'skeleton'
-    super (x, y, skeleton, { maxHP: 55, maxMP: 25, xp: 1, damage: 10, speed: 0.08, height: 1.95, width: 0.8 })
+    super (x, y, skeleton, { maxHP: 55, xp: 1, damage: 10, speed: 0.08, height: 1.95, width: 0.8 })
 
     this.animations.idle = new SpriteSet(skeleton, { name: 'idle', y: 32, w: 16, h: 32 })
     this.animations.move = new SpriteSet(skeleton, { name: 'move', y: 32, w: 16, h: 32, frames: 4, speed: 0.4 })
