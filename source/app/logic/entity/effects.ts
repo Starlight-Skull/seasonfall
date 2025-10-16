5b206217e42751d7c22bd76149a47ca45a5ddf48
Starlight-Skull <74898654+Starlight-Skull@users.noreply.github.com>
2025-10-16 22:57:58 +0200

    disabled knockback for now

--- source/app/logic/entity/effects.ts
+++ source/app/logic/entity/effects.ts
@@ -1,13 +1,15 @@
 import Entity from "../../classes/Entity";
 
 export function knockback(entity: Entity, target: Entity) {
-  if (entity.x < target.x) {
-    target.x += 1.5
-    target.y -= 0.3
-  } else {
-    target.x -= 1.5
-    target.y -= 0.3
-  }
+  // ! this way of adding knockback skips collision checks
+  // todo - disabled until vectors are added to entities
+  // if (entity.x < target.x) {
+  //   target.x += 1.5
+  //   target.y -= 0.3
+  // } else {
+  //   target.x -= 1.5
+  //   target.y -= 0.3
+  // }
 }
 
 export function heal(entity: Entity) {
